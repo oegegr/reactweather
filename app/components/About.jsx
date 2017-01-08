0c150903adec5999b8341949d7771f5e3d17d01c
George <aleksandrovgn@gmail.com>
2017-01-08 20:46:42 +0300

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,18 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <h3>About Component</h3>
+        <div>
+            <h1 className="text-center">About</h1>
+            <p>This is a weather application build on React</p>
+            <p>
+               Here are some of the tools i used: 
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+                </li>
+            </ul>
+        </div>
     );
 }
 
