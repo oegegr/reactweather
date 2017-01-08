b125f3316c5c447d0bce187448c02d0890f64eb0
George <aleksandrovgn@gmail.com>
2017-01-08 14:47:59 +0300

    added foundation; fix .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
 /**
  * Created by george on 25.12.2016.
  */
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './app/app.jsx',
+    entry: [
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx',
+    ],
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+
+    ],
     output: {
         path: __dirname,
         filename: './public/bundle.js'
